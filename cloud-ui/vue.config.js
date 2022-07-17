20e6ea3d57597bd3cb8b5def0bc9b0fa06624b8a
akadongrongyang <2511258231@qq.com>
2022-07-17 23:19:18 +0800

    no message

--- cloud-ui/vue.config.js
+++ cloud-ui/vue.config.js
@@ -4,11 +4,11 @@ module.exports = {
   assetsDir: 'static',
   productionSourceMap: false,
   devServer: {
-    host: '127.0.0.1',
-    port: 8081,
+    host: '0.0.0.0',
+    port: 8089,
     proxy: {
       "/api": {
-        target: `http://localhost:8081`,
+        target: `http://127.0.0.1:8089`,
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
